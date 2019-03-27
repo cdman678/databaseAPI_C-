424b2ba95c33203af69b7c3e999bdc745d22ee2d
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-27 13:19:09 -0500

    Update Main.cs

--- API/Main.cs
+++ API/Main.cs
@@ -1,10 +1,12 @@
 using System; 
 
 namespace Main{
-  class test { 
-    public static void Main( string[] args ){ 
-      Connect testconn = new Connect();
-      testconn.OpenConnection;
-      testconn.CloseConnection;
+  class test 
+  { 
+  public static void Main( string[] args )
+  { 
+    Connect testconn = new Connect();
+    testconn.OpenConnection;
+    testconn.CloseConnection;
   }
 }
