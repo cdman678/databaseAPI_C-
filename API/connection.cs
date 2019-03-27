4be1099d6a7dbc6f64dd94fbbff40fa0b22951d0
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-27 12:34:57 -0500

    Update connection.cs

--- API/connection.cs
+++ API/connection.cs
@@ -3,7 +3,7 @@ using MySql.Data.MySqlClient;
 
 class Connect{
 	
-	//Class variables
+    //Class variables
     private MySqlConnection connection;
     private string server;
     private string database;
@@ -23,8 +23,7 @@ class Connect{
         uid = "ubuntu";
         password = "cornisgood";
         string connectionString;
-        connectionString = "SERVER=" + server + ";" + "DATABASE=" + 
-		database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
 
         connection = new MySqlConnection(connectionString);
     }
@@ -32,69 +31,69 @@ class Connect{
     //open connection to database
     private bool OpenConnection(){
 		
-		try{
-			connection.Open();
-			return true;
-		}
-		catch (MySqlException ex){
-			//0: Connection with server can not be found   |   1045: Error with username or password
-			switch (ex.Number){		
-				case 0:
-					MessageBox.Show("Cannot find connection");
-					break;
+	try{
+		connection.Open();
+		return true;
+	}
+	catch (MySqlException ex){
+		//0: Connection with server can not be found   |   1045: Error with username or password
+		switch (ex.Number){		
+			case 0:
+				MessageBox.Show("Cannot find connection");
+				break;
 
-				case 1045:
-					MessageBox.Show("Invalid username/password");
-					break;
-			}
-			return false;
+			case 1045:
+				MessageBox.Show("Invalid username/password");
+				break;
 		}
+		return false;
+	}
     }
 
     //Close connection
     private bool CloseConnection(){
-		try{
-			connection.Close();
-			return true;
-		}
-		catch (MySqlException ex){
-			//Should display the error message generated from mySQL
-			MessageBox.Show(ex.Message);
-			return false;
-		}
+	try{
+		connection.Close();
+		return true;
+	}
+	catch (MySqlException ex){
+		//Should display the error message generated from mySQL
+		MessageBox.Show(ex.Message);
+		return false;
+	}
 
     //Insert statement
     public void Insert(){
-		//TODO
+	//TODO
     }
 
     //Update statement
     public void Update(){
-		//TODO
+	//TODO
     }
 
     //Delete statement
     public void Delete(){
-		//TODO
+	//TODO
     }
 
     //Select statement
     public List <string> [] Select(){
-		//TODO
+	//TODO
     }
 
     //Count statement
     public int Count(){
-		//TODO
+	//TODO
     }
 
     //Backup
     public void Backup(){
-		//TODO
+	//TODO
     }
 
     //Restore
     public void Restore(){
-		//TODO
+	//TODO
     }
 }
