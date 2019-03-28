711ceb2e83efb40a5b1abd667c310fd95fc50459
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-28 14:49:35 -0500

    Update Plants.cs

--- API/Plants.cs
+++ API/Plants.cs
@@ -1,5 +1,4 @@
 using System;
-//using System.Collections.Generic;   //Strings - wont need for final product
 using MySql.Data.MySqlClient;
 
 class Plants : Connect
@@ -7,30 +6,10 @@ class Plants : Connect
     //Constructor call to base case - selecting 'plants' database
     public Plants() : base("plants") { }
 
-    //This function needs to be called after each query  function to close the connection
+    //This function needs to be called after each query function to close the connection
     public new void Close() { base.Close(); }
 
-    //public wrapper for 'Select * FROM plants' in SelectALLPlants
+    //public wrapper for 'Select * FROM __' in SelectALL in Connection
     public MySqlDataReader ShowAll(string tableName) { return SelectAll(tableName); }
 
-    //Select statement
-    private MySqlDataReader SelectAll(string tableName)
-    {
-        //constructing query
-        string queryTemp = "SELECT * FROM ";
-        string query = queryTemp + tableName; 
-
-        //Open connection
-        Open();
-
-        //Create Command
-        MySqlCommand cmd = new MySqlCommand(query, connection);
-
-        //Create a data reader and Execute the command
-        MySqlDataReader dataReader = cmd.ExecuteReader();
-
-        //return the MySqlDataReader to be used
-        return (dataReader); 
-
-    }
 }
