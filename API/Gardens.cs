8e64e36cf670f83968edba0d9808ff3179f2246a
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-28 20:35:45 -0500

    Update Gardens.cs

--- API/Gardens.cs
+++ API/Gardens.cs
@@ -9,7 +9,7 @@ class Gardens : Connect
     //This function needs to be called after each query function to close the connection
     public new void Close() { base.Close(); }
 
-    //public wrapper for 'Select * FROM __' in SelectALL in Connection
+    //public wrapper for 'Select * FROM __' in SelectALL in Connection - return type may need to be changed after talking to Zack
     public MySqlDataReader ShowAll(string tableName) { return SelectAll(tableName); }
 
 }
